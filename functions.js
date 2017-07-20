dfbce833d0e227b839b71cc83042b9d2a6b5087e
Digity101 <danny.gia.an.thai@gmail.com>
2017-07-20 23:18:21 +0200

    latents+new layout

--- functions.js
+++ functions.js
@@ -1,4 +1,54 @@
 $( document ).ready(function() {
+    function latent (a){
+        var latentList= `
+                    <option>none</option>
+                    <option value="images/3.png">hp</option>
+                    <option value="images/4.png">atk</option>
+                    <option value="images/5.png">rcv</option>
+                    <option value="images/6.png">fire resist</option>
+                    <option value="images/7.png">water resist</option>
+                    <option value="images/8.png">wood resist</option>
+                    <option value="images/9.png">light resist</option>
+                    <option value="images/10.png">dark resist</option>
+                    <option value="images/11.png">auto heal</option>
+                    <option value="images/12.png">time extend</option>
+                    <option value="images/killers/1.png">dragon killer</option>
+                    <option value="images/killers/2.png">balance killer</option>
+                    <option value="images/killers/3.png">physical killer</option>
+                    <option value="images/killers/4.png">healer killer</option>
+                    <option value="images/killers/5.png">attacker killer</option>
+                    <option value="images/killers/6.png">god killer</option>
+                    <option value="images/killers/9.png">devil killer</option>
+                    <option value="images/killers/12.png">machine killer</option>
+            </select>`;
+       for(var i = 0; i<6; i++){
+        $("#"+a).append("<select name="+a+">" + latentList );
+       }
+
+    }
+
+    latent("p1_leader_latent");
+    latent("p1_sub1_latent");
+    latent("p1_sub2_latent");
+    latent("p1_sub3_latent");
+    latent("p1_sub4_latent");
+    latent("p2_leader_latent");
+    latent("p2_sub1_latent");
+    latent("p2_sub2_latent");
+    latent("p2_sub3_latent");
+    latent("p2_sub4_latent");
+    
+    $(`select`).on("change",function () {
+        var array =[]
+        $(`select[name=`+this.name+`]`).each(function(){
+            if ($(this).val() != "none"){
+            var img = "<img src="+$(this).val()+">";
+            array.push(img);
+            }
+            });
+        $("#"+this.name+"s").html(array);
+    });
+    
     $(`#Team_selection input[type=number]`).on("input",function () {
         if ($(this).val() > 0){
             var img = "<img src=http://puzzledragonx.com/en/img/book/"+$(this).val()+".png>";
@@ -86,8 +136,9 @@ $( document ).ready(function() {
         }
         
     });
-    
-          
+    $(`input[name=GuideName]`).on("input",function () {
+        $("#name").html(this.value);
+    });          
 });
 
 //hijason
